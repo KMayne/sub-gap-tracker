b9823edcf8c46acb580d5fa5678f5ac13e861ba6
Kian Mayne <git@kianmayne.co.uk>
2019-03-12 15:53:14 +0000

    Fixed error handling & indentation

--- tracker.py
+++ tracker.py
@@ -4,25 +4,25 @@ from datetime import datetime
 import os
 
 def refresh_sub_gap():
-	pewds = int(requests.get('https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw').text)
-	tseries = int(requests.get('https://bastet.socialblade.com/youtube/lookup?query=UCq-Fj5jknLsUf-MWSy4_brA').text)
-	sub_gap = pewds - tseries
+    pewds = int(requests.get('https://bastet.socialblade.com/youtube/lookup?query=UC-lHJZR3Gqxm24_Vd_AJ5Yw').text)
+    tseries = int(requests.get('https://bastet.socialblade.com/youtube/lookup?query=UCq-Fj5jknLsUf-MWSy4_brA').text)
+    sub_gap = pewds - tseries
 
-	print("[%s] %d" % (datetime.today().isoformat(), sub_gap))
+    print("[%s] %d" % (datetime.today().isoformat(), sub_gap))
 
-	limit = int(os.environ['SUB_GAP_ALERT_LIMIT'])
-	if (sub_gap < limit):
-		requests.post('https://maker.ifttt.com/trigger/sub_gap/with/key/{0}'.format(os.environ['IFTTT_WEBHOOK_KEY']), data = { 'value1': "{:.1f}k".format(limit / 1000.0), 'value2': sub_gap });
-		print("[%s] %d" % (datetime.today().isoformat(), "Sent notification"))
+    limit = int(os.environ['SUB_GAP_ALERT_LIMIT'])
+    if (sub_gap < limit):
+        requests.post('https://maker.ifttt.com/trigger/sub_gap/with/key/{0}'.format(os.environ['IFTTT_WEBHOOK_KEY']), data = { 'value1': "{:.1f}k".format(limit / 1000.0), 'value2': sub_gap });
+        print("[%s] %d" % (datetime.today().isoformat(), "Sent notification"))
 
 def retry(func, max_attempts):
-	attempt = 1
-	while(attempt <= max_attempts):
-                attempt += 1
-		try:
-			return func()
-		except:
-			print("Attempt #{} / {} failed".format(attempt, max_attempts))
-	print ("Giving up")
+    attempt = 1
+    while(attempt <= max_attempts):
+        try:
+            return func()
+        except Exception as err:
+            print("Attempt #{} / {} failed:\n{}".format(attempt, max_attempts, err))
+            attempt += 1
+    print ("Giving up")
 
 retry(refresh_sub_gap, 3)
