5ce6d9854899b1df99614789361c947edc4ed0e3
Kian Mayne <git@kianmayne.co.uk>
2019-03-14 14:08:08 +0000

    Fix format string

--- tracker.py
+++ tracker.py
@@ -13,7 +13,7 @@ def refresh_sub_gap():
     limit = int(os.environ['SUB_GAP_ALERT_LIMIT'])
     if (sub_gap < limit):
         requests.post('https://maker.ifttt.com/trigger/sub_gap/with/key/{0}'.format(os.environ['IFTTT_WEBHOOK_KEY']), data = { 'value1': "{:.1f}k".format(limit / 1000.0), 'value2': sub_gap });
-        print("[%s] %d" % (datetime.today().isoformat(), "Sent notification"))
+        print("[%s] %s" % (datetime.today().isoformat(), "Sent notification"))
 
 def retry(func, max_attempts):
     attempt = 1
